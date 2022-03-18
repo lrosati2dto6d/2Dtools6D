73190201b874e046f7cbe2a421e20e98d185eda6
lrosati2dto6d <l.rosati@2dto6d.com>
2022-03-18 18:00:46 +0100

    update tool pomodoro

--- 2Dtools6D.tab/Misc.Panel/Pomodoro.pushbutton/script.py
+++ 2Dtools6D.tab/Misc.Panel/Pomodoro.pushbutton/script.py
@@ -24,15 +24,28 @@ app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
+valueuniquitem = forms.ask_for_one_item(
+    ['5 Minutes', '10 Minutes', '25 Minutes'],
+    default='25 Minutes',
+    prompt='Select Pomodoro Time',
+    title='POMODORO TECHNIQUE')
+
+
+
 output = script.get_output()
 
-max_value = 1000
+if valueuniquitem == '5 Minutes':
+	max_value = 15000
+elif valueuniquitem == '10 Minutes':
+	max_value = 30000
+else:
+	max_value = 75000
 
 with ProgressBar(title= 'POMODORO TECHNIQUE',cancellable=True,step=1) as pb:
 	for counter in range(0,max_value):
 		if pb.cancelled:
-			forms.alert('You should not stop', exitscript=True)
+			forms.alert('You should Not stop!!! Go back to work', exitscript=True)
 		else:
 			pb.update_progress(counter,max_value)
 
-forms.alert('Good Job!!! You Deserve 5 Minutes of Coffee Break!!!', exitscript=True)
+forms.alert('GOOD JOB!!! You Deserve 5 Minutes of Coffee Break!!!', exitscript=True)
