f7404a9a6ef8b2e3bf3012e060c59e4ca9f1a554
lrosati2dto6d <l.rosati@2dto6d.com>
2023-09-04 17:43:35 +0200

    Aggiunta Tasto Compilazione IMP

---
+++ AnasL4.tab/Check.panel/CompilazioneImpianti.pushbutton/script.py
@@ -0,0 +1,122 @@
+""" Compila parametri di default degli elementi appartenenti al workset Impianti """
+
+__title__= 'Valorizzazione Default\nImpianti'
+__author__= 'Roberto Dolfini'
+
+import sys
+import os
+import csv
+import clr
+import System
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+doc =  DocumentManager.Instance.CurrentDBDocument
+ 
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+ 
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import *
+
+
+
+import pyrevit
+from pyrevit import script
+from pyrevit import forms
+#import rpw.ui.forms
+#from rpw.ui.forms import (FlexForm, Label, Separator, Button, CheckBox)
+
+
+def EstraiCodici(elemento):
+	name = elemento.LookupParameter("Famiglia").AsValueString()
+
+	Opera = name.split(".")[0]
+	Parteopera = name.split(".")[2]
+	Elemento = name.split(".")[3][:3]
+	Struttura = name.split(".")[1]
+
+	return Opera,Parteopera,Elemento,Struttura
+
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+activeView = doc.ActiveView
+Output = script.get_output()
+
+t_AssegnazioneParametriIstanza = Transaction(doc,"Assegnazione parametri impianti di istanza")
+t_AssegnazioneParametriTipo = Transaction(doc,"Assegnazione parametri impianti di tipo")
+
+## ESTRAZIONE ELEMENTI SU WORKSET IMPIANTI
+
+CollectorWorkset = FilteredWorksetCollector(doc).ToWorksets()
+
+for w in CollectorWorkset:
+	if w.Name == "08_IMP":
+		wksetId = w.Id		
+
+filter = ElementWorksetFilter(wksetId)
+
+ImpiantiInView = FilteredElementCollector(doc,activeView.Id).WhereElementIsNotElementType().WherePasses(filter).ToElements()
+
+
+## LISTE DI RIFERIMENTO
+
+GEO_Quota_sensore = "ACC,IFS,SCA,SEM,TEC,TCM,VCM,TSS,TIG,SUM,CLI,BRE,IDO"
+ANA_Progettista = "ALI,BLI,CAB,CNP,CAE,CAV,COL,COM,CEE,DIE,IMT,INS,INM,MUL,PLI,PZE,QEB,QEM,REL,REP,RIF,SCS,SDE,TRS"
+TEC_Numero_seriale = "IFS,TEC,TCM,ACC,SCA,VCM,TSS,SEM,TIG,SUM,CLI,BRE,IDO"
+TEC_Posizione = "ACC,IFS,SCA,SEM,TEC,TCM,VCM,TIG,SUM"
+
+## ASSEGNAZIONE PARAMETRI DI ISTANZA
+
+t_AssegnazioneParametriIstanza.Start()
+
+CodiceElemento = []
+for elemento in ImpiantiInView:
+	CodiceElemento.append(elemento.LookupParameter("Famiglia").AsValueString().split(".")[3][:3])
+
+for elemento,codice in zip(ImpiantiInView,CodiceElemento):
+
+	elemento.LookupParameter("IDE_LOR").Set("BASSO") # Valore di default da assegnare a tutti
+	
+	if codice in GEO_Quota_sensore:
+		elemento.LookupParameter("GEO_Quota sensore").Set(elemento.LookupParameter("Quota altimetrica da livello").AsDouble())
+
+	if codice in ANA_Progettista:
+		elemento.LookupParameter("ANA_Progettista").Set("RINA Consulting SpA")
+
+	if codice in TEC_Numero_seriale:
+		elemento.LookupParameter("TEC_Numero seriale").Set("-")
+
+	if codice in TEC_Posizione:
+		host = elemento.Host
+		try:
+			elemento.LookupParameter("TEC_Posizione").Set("CAM." + host.LookupParameter("INF_Campata di appartenenza").AsString())
+		except:
+			elemento.LookupParameter("TEC_Posizione").Set(host.LookupParameter("IDE_Gruppo anagrafica").AsString()[6:12])
+
+t_AssegnazioneParametriIstanza.Commit()
+
+## ASSEGNAZIONE PARAMETRI DI TIPO
+
+t_AssegnazioneParametriTipo.Start()
+
+for elemento in ImpiantiInView:
+
+	Codici = EstraiCodici(elemento)
+
+	doc.GetElement(elemento.GetTypeId()).LookupParameter("Modello").Set(Codici[0])
+	doc.GetElement(elemento.GetTypeId()).LookupParameter("Commenti sul tipo").Set(Codici[1])
+	doc.GetElement(elemento.GetTypeId()).LookupParameter("Descrizione").Set(Codici[2])
+
+	if "XXX" not in elemento.LookupParameter("Famiglia").AsValueString():
+		doc.GetElement(elemento.GetTypeId()).LookupParameter("Contrassegno tipo").Set(Codici[3])
+	else:
+		doc.GetElement(elemento.GetTypeId()).LookupParameter("Contrassegno tipo").Set("")
+
+t_AssegnazioneParametriTipo.Commit()
+
+pyrevit.forms.toaster.send_toast("Compilazione effettuata", title = "Compilazione Default Impianti", icon = sys.path[0] + "/iconanera.png")
